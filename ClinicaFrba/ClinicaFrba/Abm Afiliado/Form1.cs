4be9fc4d3f4b6577df55d85366f6323f9bcd1452
GuidoOliveri <guido.oli@hotmail.com>
2016-10-25 17:58:04 -0300

    Agrego pantalla de alta de afiliados

--- ClinicaFrba/ClinicaFrba/Abm Afiliado/Form1.cs
+++ ClinicaFrba/ClinicaFrba/Abm Afiliado/Form1.cs
@@ -16,5 +16,17 @@ namespace ClinicaFrba.Abm_Afiliado
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
     }
 }
