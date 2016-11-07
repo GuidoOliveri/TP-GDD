b5f7cd5d67e3935855c62e02ee3fc23b1889af8c
Tenmaster <tenlion@hotmail.com>
2016-11-07 07:23:44 -0300

    Agrego Modificar Afiliado procedure

--- ClinicaFrba/ClinicaFrba/Abm Afiliado/frmModificarAfiliado.cs
+++ ClinicaFrba/ClinicaFrba/Abm Afiliado/frmModificarAfiliado.cs
@@ -13,20 +13,35 @@ namespace ClinicaFrba.Abm_Afiliado
     public partial class frmModificarAfiliado : Form {
         
         private string comando = "";
-    
-        public frmModificarAfiliado(Clases.BaseDeDatosSQL bdd, int idPersona)
+        Clases.BaseDeDatosSQL bdd;
+
+        int idAfiliado;
+
+        private String direccion = "";
+        private String telefono = "";
+        private String mail = "";
+
+        public frmModificarAfiliado(Clases.BaseDeDatosSQL bdd, int idAfiliado)
         {
             InitializeComponent();
             
-            bdd = new Clases.BaseDeDatosSQL(); // Hardcode de prueba
-            
-            idPersona = 1123960; // Hardcode de prueba
+            this.bdd = new Clases.BaseDeDatosSQL(); // Hardcode de prueba
+
+            this.idAfiliado = idAfiliado;
+
+            this.idAfiliado = 112396001;
+            int idPersona = 1123960; // Hardcode de prueba
             comando = "select * from GD2C2016.NEXTGDD.Persona where id_persona ="+idPersona;
-            System.Collections.ArrayList persona = bdd.obtenerRow(comando);
-            
-            txtDir.Text = (string) persona[3];
-            txtTel.Text = persona[4].ToString();
-            txtMail.Text = (string)persona[5];
+            System.Collections.ArrayList persona = this.bdd.obtenerRow(comando);
+
+            lblNombreAfiliado.Text = persona[1].ToString() +" "+ persona[2].ToString();
+            direccion = (string)persona[3];
+            telefono = persona[4].ToString();
+            mail = (string)persona[5];
+
+            txtDir.Text = direccion;
+            txtMail.Text = mail;
+            txtTel.Text = telefono;
             
             
         }
@@ -43,12 +58,32 @@ namespace ClinicaFrba.Abm_Afiliado
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
+            if (txtDir.Text != direccion)
+            {
+                comando = "EXECUTE NEXTGDD.modificar_Afiliado_Domic @id='" + idAfiliado + "',@nuevo_dom='" + txtDir.Text;
+                bdd.ExecStoredProcedure2(comando);
+                MessageBox.Show("Actualizacion exitosa!", "Actualizacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
+            }
+            if (txtTel.Text != telefono)
+            {
+                comando = "EXECUTE NEXTGDD.modificar_Afiliado_Telef @id='" + idAfiliado + "',@nuevo_nuevo_telef='" + txtTel.Text;
+                bdd.ExecStoredProcedure2(comando);
+                MessageBox.Show("Actualizacion exitosa!", "Actualizacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            if (txtMail.Text != mail)
+            {
+                comando = "EXECUTE NEXTGDD.modificar_Afiliado_Mail @id='" + idAfiliado + "',@nuevo_mail='" + txtDir.Text;
+                bdd.ExecStoredProcedure2(comando);
+                MessageBox.Show("Actualizacion exitosa!", "Actualizacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
-           
+            this.Dispose();
         }
 
         private void txtDir_TextChanged(object sender, EventArgs e)
