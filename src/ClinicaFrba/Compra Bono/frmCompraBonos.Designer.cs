c33a75552279960be5bdc05e01464dce5c2db14c
Tenmaster <tenlion@hotmail.com>
2016-11-13 02:25:26 -0300

    Logica comprar bono

--- src/ClinicaFrba/Compra Bono/frmCompraBonos.Designer.cs
+++ src/ClinicaFrba/Compra Bono/frmCompraBonos.Designer.cs
@@ -30,17 +30,24 @@
         {
             this.lblCantBonos = new System.Windows.Forms.Label();
             this.btnAceptar = new System.Windows.Forms.Button();
-            this.txtCantBonos = new System.Windows.Forms.TextBox();
             this.lblPrecioBonos = new System.Windows.Forms.Label();
-            this.txtPrecioBonos = new System.Windows.Forms.TextBox();
             this.lblSignoPesos = new System.Windows.Forms.Label();
             this.cmdVolver = new System.Windows.Forms.Button();
+            this.txtPrecioBono = new System.Windows.Forms.Label();
+            this.txtNumeroAfiliado = new System.Windows.Forms.TextBox();
+            this.lblNumeroAfiliado = new System.Windows.Forms.Label();
+            this.btnCargarAfiliado = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotalAPagar = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cantBonosUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.cantBonosUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // lblCantBonos
             // 
             this.lblCantBonos.AutoSize = true;
-            this.lblCantBonos.Location = new System.Drawing.Point(12, 85);
+            this.lblCantBonos.Location = new System.Drawing.Point(15, 91);
             this.lblCantBonos.Name = "lblCantBonos";
             this.lblCantBonos.Size = new System.Drawing.Size(186, 13);
             this.lblCantBonos.TabIndex = 0;
@@ -49,41 +56,28 @@
             // 
             // btnAceptar
             // 
-            this.btnAceptar.Location = new System.Drawing.Point(95, 133);
+            this.btnAceptar.Location = new System.Drawing.Point(90, 141);
             this.btnAceptar.Name = "btnAceptar";
             this.btnAceptar.Size = new System.Drawing.Size(75, 23);
             this.btnAceptar.TabIndex = 1;
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = true;
-            this.btnAceptar.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // txtCantBonos
-            // 
-            this.txtCantBonos.Location = new System.Drawing.Point(239, 85);
-            this.txtCantBonos.Name = "txtCantBonos";
-            this.txtCantBonos.Size = new System.Drawing.Size(164, 20);
-            this.txtCantBonos.TabIndex = 2;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
             // lblPrecioBonos
             // 
             this.lblPrecioBonos.AutoSize = true;
-            this.lblPrecioBonos.Location = new System.Drawing.Point(12, 52);
+            this.lblPrecioBonos.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lblPrecioBonos.Location = new System.Drawing.Point(17, 63);
             this.lblPrecioBonos.Name = "lblPrecioBonos";
             this.lblPrecioBonos.Size = new System.Drawing.Size(182, 13);
             this.lblPrecioBonos.TabIndex = 3;
             this.lblPrecioBonos.Text = "Precio del bono según el plan actual:";
             // 
-            // txtPrecioBonos
-            // 
-            this.txtPrecioBonos.Location = new System.Drawing.Point(346, 52);
-            this.txtPrecioBonos.Name = "txtPrecioBonos";
-            this.txtPrecioBonos.Size = new System.Drawing.Size(57, 20);
-            this.txtPrecioBonos.TabIndex = 4;
-            // 
             // lblSignoPesos
             // 
             this.lblSignoPesos.AutoSize = true;
-            this.lblSignoPesos.Location = new System.Drawing.Point(327, 55);
+            this.lblSignoPesos.Location = new System.Drawing.Point(198, 64);
             this.lblSignoPesos.Name = "lblSignoPesos";
             this.lblSignoPesos.Size = new System.Drawing.Size(13, 13);
             this.lblSignoPesos.TabIndex = 5;
@@ -91,24 +85,107 @@
             // 
             // cmdVolver
             // 
-            this.cmdVolver.Location = new System.Drawing.Point(249, 133);
+            this.cmdVolver.Location = new System.Drawing.Point(264, 140);
             this.cmdVolver.Name = "cmdVolver";
-            this.cmdVolver.Size = new System.Drawing.Size(69, 22);
+            this.cmdVolver.Size = new System.Drawing.Size(75, 23);
             this.cmdVolver.TabIndex = 6;
             this.cmdVolver.Text = "Volver";
             this.cmdVolver.UseVisualStyleBackColor = true;
             this.cmdVolver.Click += new System.EventHandler(this.cmdVolver_Click);
             // 
+            // txtPrecioBono
+            // 
+            this.txtPrecioBono.AutoSize = true;
+            this.txtPrecioBono.Location = new System.Drawing.Point(217, 64);
+            this.txtPrecioBono.Name = "txtPrecioBono";
+            this.txtPrecioBono.Size = new System.Drawing.Size(28, 13);
+            this.txtPrecioBono.TabIndex = 7;
+            this.txtPrecioBono.Text = "0.00";
+            // 
+            // txtNumeroAfiliado
+            // 
+            this.txtNumeroAfiliado.Location = new System.Drawing.Point(117, 30);
+            this.txtNumeroAfiliado.Name = "txtNumeroAfiliado";
+            this.txtNumeroAfiliado.Size = new System.Drawing.Size(197, 20);
+            this.txtNumeroAfiliado.TabIndex = 9;
+            this.txtNumeroAfiliado.Visible = false;
+            // 
+            // lblNumeroAfiliado
+            // 
+            this.lblNumeroAfiliado.AutoSize = true;
+            this.lblNumeroAfiliado.Location = new System.Drawing.Point(14, 33);
+            this.lblNumeroAfiliado.Name = "lblNumeroAfiliado";
+            this.lblNumeroAfiliado.Size = new System.Drawing.Size(96, 13);
+            this.lblNumeroAfiliado.TabIndex = 8;
+            this.lblNumeroAfiliado.Text = "Numero de Afiliado";
+            this.lblNumeroAfiliado.Visible = false;
+            // 
+            // btnCargarAfiliado
+            // 
+            this.btnCargarAfiliado.Location = new System.Drawing.Point(331, 27);
+            this.btnCargarAfiliado.Name = "btnCargarAfiliado";
+            this.btnCargarAfiliado.Size = new System.Drawing.Size(75, 23);
+            this.btnCargarAfiliado.TabIndex = 10;
+            this.btnCargarAfiliado.Text = "Cargar";
+            this.btnCargarAfiliado.UseVisualStyleBackColor = true;
+            this.btnCargarAfiliado.Click += new System.EventHandler(this.btnCargarAfiliado_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Total:";
+            // 
+            // lblTotalAPagar
+            // 
+            this.lblTotalAPagar.AutoSize = true;
+            this.lblTotalAPagar.Location = new System.Drawing.Point(335, 92);
+            this.lblTotalAPagar.Name = "lblTotalAPagar";
+            this.lblTotalAPagar.Size = new System.Drawing.Size(28, 13);
+            this.lblTotalAPagar.TabIndex = 13;
+            this.lblTotalAPagar.Text = "0.00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(316, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "$";
+            // 
+            // cantBonosUpDown
+            // 
+            this.cantBonosUpDown.Location = new System.Drawing.Point(207, 89);
+            this.cantBonosUpDown.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.cantBonosUpDown.Name = "cantBonosUpDown";
+            this.cantBonosUpDown.Size = new System.Drawing.Size(64, 20);
+            this.cantBonosUpDown.TabIndex = 14;
+            this.cantBonosUpDown.ValueChanged += new System.EventHandler(this.cantBonos_ValueChanged);
+            // 
             // frmCompraBonos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 181);
+            this.ClientSize = new System.Drawing.Size(430, 187);
+            this.Controls.Add(this.cantBonosUpDown);
+            this.Controls.Add(this.lblTotalAPagar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCargarAfiliado);
+            this.Controls.Add(this.txtNumeroAfiliado);
+            this.Controls.Add(this.lblNumeroAfiliado);
+            this.Controls.Add(this.txtPrecioBono);
             this.Controls.Add(this.cmdVolver);
             this.Controls.Add(this.lblSignoPesos);
-            this.Controls.Add(this.txtPrecioBonos);
             this.Controls.Add(this.lblPrecioBonos);
-            this.Controls.Add(this.txtCantBonos);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.lblCantBonos);
             this.Name = "frmCompraBonos";
@@ -116,6 +193,7 @@
             this.Text = "Compra de Bonos";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmCompraBonos_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cantBonosUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,10 +203,16 @@
 
         private System.Windows.Forms.Label lblCantBonos;
         private System.Windows.Forms.Button btnAceptar;
-        private System.Windows.Forms.TextBox txtCantBonos;
         private System.Windows.Forms.Label lblPrecioBonos;
-        private System.Windows.Forms.TextBox txtPrecioBonos;
         private System.Windows.Forms.Label lblSignoPesos;
         private System.Windows.Forms.Button cmdVolver;
+        private System.Windows.Forms.Label txtPrecioBono;
+        private System.Windows.Forms.TextBox txtNumeroAfiliado;
+        private System.Windows.Forms.Label lblNumeroAfiliado;
+        private System.Windows.Forms.Button btnCargarAfiliado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotalAPagar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown cantBonosUpDown;
     }
 }
--- src/ClinicaFrba/Compra Bono/frmCompraBonos.cs
+++ src/ClinicaFrba/Compra Bono/frmCompraBonos.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace ClinicaFrba.Compra_Bono
 {
@@ -15,6 +16,8 @@ namespace ClinicaFrba.Compra_Bono
         private string rol = "";
         private string usuario = "";
         private Clases.BaseDeDatosSQL bdd;
+        private string idAfiliado = "";
+
 
         public frmCompraBonos(string rol, string usuario, Clases.BaseDeDatosSQL bdd)
         {
@@ -22,6 +25,24 @@ namespace ClinicaFrba.Compra_Bono
             this.rol = rol;
             this.usuario = usuario;
             this.bdd = bdd;
+
+            if(rol.Equals("Administrativo")){
+                txtNumeroAfiliado.Visible = true;
+                lblNumeroAfiliado.Visible = true;
+            }
+            else if (rol.Equals("Afiliado"))
+            {
+                idAfiliado = "124453901"; // Debe ser cargado del afiliado si 
+                txtPrecioBono.Text = obtenerPrecioBono(txtNumeroAfiliado.Text);
+
+            }
+
+        }
+
+        private String obtenerPrecioBono(string afiliado)
+        {
+            String queryString = "SELECT precio_bono_consulta FROM NEXTGDD.Afiliado RIGHT JOIN NEXTGDD.Plan_Medico ON Afiliado.cod_plan = Plan_Medico.cod_plan WHERE Afiliado.nro_afiliado ='" + txtNumeroAfiliado.Text + "'";
+            return bdd.buscarCampo(queryString);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -33,13 +54,7 @@ namespace ClinicaFrba.Compra_Bono
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            frmMontoAPagar monto = new frmMontoAPagar(rol,usuario,bdd);
-            this.Hide();
-            monto.Show();
-        }
-
+     
         private void cmdVolver_Click(object sender, EventArgs e)
         {
             Login.frmMenuDeAbms menuAbm = new Login.frmMenuDeAbms(rol, usuario, bdd);
@@ -57,6 +72,83 @@ namespace ClinicaFrba.Compra_Bono
             {
                 e.Cancel = true;
             }
+        }
+
+
+        private void txtCantBonos_TextChanged(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        private void btnCargarAfiliado_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtPrecioBono.Text = obtenerPrecioBono(txtNumeroAfiliado.Text);
+            }
+            catch 
+            {
+                MessageBox.Show("No Se Encontro al afiliado", "Buscar Afiliado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        
+            }
+            
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+            String comando;
+
+            comando = "select cod_plan from NEXTGDD.Afiliado where nro_afiliado = '" + idAfiliado + "'";
+            String plan = bdd.buscarCampo(comando);
+
+            //String fecha = System.Configuration.ConfigurationSettings.AppSettings["date"];
+
+            // Crear Bonos por 
+            for (int i = 0; i < cantBonosUpDown.Value; i++)
+            {
+                comando = "EXECUTE NEXTGDD.comprarBono @fechaImpresion='" + convertirFecha("7/11/2016") + "', @compraFecha='" + convertirFecha("7/11/2016") + "', @codPlan='" + plan + "', @nroAfiliado=" + idAfiliado + "'";
+                bdd.ExecStoredProcedure2(comando);
+            }
+
+
+            // Registrar la compra
+            comando = "EXECUTE NEXTGDD.registrarCompra @cant='" + cantBonosUpDown.Value + "', @idAfiliado='" + idAfiliado + "', @precioTotal='" + lblTotalAPagar.Text + "'";
+            bdd.ExecStoredProcedure2(comando);
+
+            MessageBox.Show("Compra completada con exito!", "Compra", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+        }
+
+        private void cantBonos_ValueChanged(object sender, EventArgs e)
+        {
+            if (cantBonosUpDown.Value == 0)
+            {
+                lblTotalAPagar.Text = "0.00";
+            }
+            else
+            {
+                // lblTotalAPagar.Text = cantBonosUpDown.Value * int.Parse(lblPrecioBonos.Text); 
+            }
+        }
+
+        private string convertirFecha(string fecha)
+        {
+            string fechaSinTiempo = fecha.Split(' ')[0];
+            string dia = fechaSinTiempo.Split('/')[0];
+            if (dia.Length == 1)
+            {
+                dia = '0' + dia;
+            }
+            string mes = fechaSinTiempo.Split('/')[1];
+            if (mes.Length == 1)
+            {
+                mes = '0' + mes;
+            }
+            string año = fechaSinTiempo.Split('/')[2];
+            return año + "/" + mes + "/" + dia + " " + fecha.Split(' ')[1];
         }
     }
 }
