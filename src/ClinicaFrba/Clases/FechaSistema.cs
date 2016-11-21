94e3063537efc7cad96099d983dea86d98cef989
Tenmaster <tenlion@hotmail.com>
2016-11-21 04:20:28 -0300

    Agrego convertirFecha a Clases.Fecha

--- src/ClinicaFrba/Clases/FechaSistema.cs
+++ src/ClinicaFrba/Clases/FechaSistema.cs
@@ -9,5 +9,22 @@ namespace ClinicaFrba.Clases
     class FechaSistema
     {
         public static string fechaSistema { set; get; }
+
+        public static string fechaActualConvertida()
+        {
+            string fechaSinTiempo = fechaSistema.Split(' ')[0];
+            string dia = fechaSinTiempo.Split('/')[0];
+            if (dia.Length == 1)
+            {
+                dia = '0' + dia;
+            }
+            string mes = fechaSinTiempo.Split('/')[1];
+            if (mes.Length == 1)
+            {
+                mes = '0' + mes;
+            }
+            string año = fechaSinTiempo.Split('/')[2];
+            return año + "/" + mes + "/" + dia + " " + fechaSistema.Split(' ')[1];
+        }
     }
 }
